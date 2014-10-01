11dfb7de96fb346105c0e965f7913db3e4c6f186
Stefan Trauth <mail@stefantrauth.de>
2014-10-01 11:46:43 +0200

    add more required packages

--- cookbooks-source/main/recipes/default.rb
+++ cookbooks-source/main/recipes/default.rb
@@ -7,18 +7,30 @@
 # All rights reserved - Do Not Redistribute
 #
 
+# packages for curl gem dependencies
+package "libcurl3"
+package "libcurl3-gnutls"
+package "libcurl4-openssl-dev"
+package "libcurl4-gnutls-dev"
+# package for sqlite3 gem dependencies
+package "libsqlite3-dev"
+
 # install some packages for daily work
 package "vim"
 package "screen"
 package "tmux"
 package "htop"
+package "mosh"
 
 # packages for reliveradio required gems
 package "imagemagick"
 package "libtag1-dev"
 
+# remove apache if it was preinstalled on the vps image
 package "apache2" do
 	action :remove
+end
+
 package "mpd"
 # disable the service that is installed by the package
 service "mpd" do
